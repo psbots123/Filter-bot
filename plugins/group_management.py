99ac8272e57e29bd78f7c423832406a335c33407
psbots123 <155368022+psbots123@users.noreply.github.com>
2024-02-05 15:55:14 +0530

    Update group_management.py

--- plugins/group_management.py
+++ plugins/group_management.py
@@ -15,7 +15,48 @@ async def members_management(client, message):
     InlineKeyboardButton('Kick Deleted Accounts', callback_data=f'kick_deleted_accounts_members')
   ]]
   await message.reply_text("Select one of function to manage members.", reply_markup=InlineKeyboardMarkup(btn))
-  
+
+@Client.on_message(filters.command('promote') & filters.group)
+async def add_group_admin(client, message):
+    if not await is_check_admin(client, message.chat.id, message.from_user.id):
+        return await message.reply_text('You are not an admin in this group.')
+
+    if message.reply_to_message and message.reply_to_message.from_user:
+        user_id = message.reply_to_message.from_user.id
+    else:
+        try:
+            user_id = message.text.split(" ", 1)[1]
+        except IndexError:
+            return await message.reply_text("Reply to any user message or provide the user id or username.")
+
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        pass
+
+    try:
+        user = (await client.get_chat_member(message.chat.id, user_id)).user
+    except:
+        return await message.reply_text("Can't find the given user in this group.")
+    permissions = ChatPermissions(
+        can_send_messages=True,
+        can_send_media_messages=True,
+        can_send_polls=True,
+        can_send_other_messages=True,
+        can_add_web_page_previews=True,
+        can_change_info=True,
+        can_invite_users=True,
+        can_pin_messages=True,
+        )
+
+    
+    try:
+        await client.promote_chat_member(message.chat.id, user_id,permissions=permissions)
+    except:
+        return await message.reply_text("I don't have access to add admin.")
+
+    await message.reply_text(f'Successfully added {user.mention} as admin to {message.chat.title}')
+
   
 @Client.on_message(filters.command('ban') & filters.group)
 async def ban_chat_user(client, message):
